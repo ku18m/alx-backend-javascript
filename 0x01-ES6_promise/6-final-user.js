80cedf32ada86ff9356c266aae5f87e3a4bbf9e4
Mohamed Kamal <ku18mz@gmail.com>
2024-04-25 04:48:11 +0200

    code fail, return a dict with given keys

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,4 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default async (firstName, lastName, fileName) => Promise.allSettled([
   uploadPhoto(fileName), signUpUser(firstName, lastName)])
-  .then((values) => values.map((value) => (value.status === 'fulfilled' ? value.value : value.reason)));
+  .then((values) => values.map((value) => (
+    {
+      status: value.status,
+      value: value.status === 'fulfilled' ? value.value : value.reason,
+    })));
