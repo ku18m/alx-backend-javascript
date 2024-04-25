0d703bab09b3e57abf41532af92b7b7b87bc7a32
Mohamed Kamal <ku18mz@gmail.com>
2024-04-25 20:39:34 +0200

    task 1, use es6 class keyword instead of class function

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,3 +1,5 @@
-export default function (maxStudentsSize) {
-  this._maxStudentsSize = maxStudentsSize;
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
