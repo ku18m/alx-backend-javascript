ae93ed382214381dc3624dbc489e796dfc9d17cf
Mohamed Kamal <ku18mz@gmail.com>
2024-04-25 04:43:23 +0200

    on error return the reason message instead of error object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,4 +3,4 @@ import uploadPhoto from './5-photo-reject';
 
 export default async (firstName, lastName, fileName) => Promise.allSettled([
   uploadPhoto(fileName), signUpUser(firstName, lastName)])
-  .then((values) => values);
+  .then((values) => values.map((value) => (value.status === 'fulfilled' ? value.value : value.reason)));
