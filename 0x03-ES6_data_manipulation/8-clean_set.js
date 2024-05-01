10130e7002b695b81bdf21461339a31958c42196
Mohamed Kamal <ku18mz@gmail.com>
2024-05-01 05:41:05 +0300

    check parameters types

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,8 @@
 export default function (set, value) {
-  return value !== '' ? Array.from(set).filter((val) => val.startsWith(value)).map((val) => val.slice(value.length)).join('-') : '';
+  if (
+    !set && !value && !(set instanceof Set) && typeof value !== 'string' && value === ''
+  ) {
+    return '';
+  }
+  return Array.from(set).filter((val) => val.startsWith(value)).map((val) => val.slice(value.length)).join('-');
 }
